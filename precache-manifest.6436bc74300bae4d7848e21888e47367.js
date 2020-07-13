d451f6a9f619cd9a415b70e97a28a0e86031fc44
bharathjinka09 <bharathjinka09@gmail.com>
2020-07-13 16:35:52 +0530

    Updates

---
+++ precache-manifest.6436bc74300bae4d7848e21888e47367.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "751d48543ba0f76e45b3e6033530429f",
+    "url": "/messenger-clone/index.html"
+  },
+  {
+    "revision": "8e6ac45ec6a54432ca40",
+    "url": "/messenger-clone/static/css/main.82ddc1bb.chunk.css"
+  },
+  {
+    "revision": "37066d423428b82bdb1d",
+    "url": "/messenger-clone/static/js/2.65757f22.chunk.js"
+  },
+  {
+    "revision": "74c0ddca854ac003128884c6311253d0",
+    "url": "/messenger-clone/static/js/2.65757f22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e6ac45ec6a54432ca40",
+    "url": "/messenger-clone/static/js/main.6be1aa00.chunk.js"
+  },
+  {
+    "revision": "f6549375430d97ce905c",
+    "url": "/messenger-clone/static/js/runtime-main.d7b8290c.js"
+  }
+]);
